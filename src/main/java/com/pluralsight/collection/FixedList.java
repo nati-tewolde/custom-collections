1fdef453aa3ddaf9d4f5948af7bb1fbff73d0f58
nati-tewolde <natisnewemail@gmail.com>
2025-11-03 11:48:30 -0500

    Add FixedList generic class

---
+++ src/main/java/com/pluralsight/collection/FixedList.java
@@ -0,0 +1,27 @@
+package com.pluralsight.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FixedList<T> {
+    private List<T> items;
+    private int maxSize;
+
+    public FixedList(int maxSize) {
+        this.items = new ArrayList<>();
+        this.maxSize = maxSize;
+    }
+
+    public void add(T item) {
+        if (items.size() >= maxSize) {
+            throw new IllegalStateException("List has reached maximum capacity.");
+        } else {
+            items.add(item);
+        }
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+
+}
